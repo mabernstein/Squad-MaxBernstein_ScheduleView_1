c7942adbf39ffb8998a93cb2c03c2a8262bc724f
Max Bernstein <maxbstl@yohoo.com>
2018-11-08 17:50:08 -0600

    Initial Commit

---
+++ Squad-ScheduleView_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Squad-ScheduleView_1
+//
+//  Created by Max Bernstein on 11/8/18.
+//  Copyright © 2018 Max Bernstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
